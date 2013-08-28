7239e3195868b64c42bd6a3e8170cd930a001f29
Stefan Aichholzer <play@analogbird.com>
2013-08-28 15:59:14 +0200

    Session getID instead of the clutter.

--- phetiche/core/utilities/phetiche_session.php
+++ phetiche/core/utilities/phetiche_session.php
@@ -2,36 +2,30 @@
 
 class Phetiche_session {
 
-	private static $session = NULL;
-
-	public function __construct($lifetime = 1200, $cookie_life = 1200)
+	/**
+	 * 691200 seconds = 8 days
+	 *
+	 * @param int $lifetime
+	 * @param int $cookie_life
+	 */
+	private static function init($lifetime = 691200, $cookie_life = 691200)
 	{
-		ini_set('session.gc_probability', 1);
 		ini_set('session.gc_divisor', 1);
-		ini_set('session.gc_maxlifetime', 1200);
-		ini_set('session.cookie_lifetime', 1200);
+		ini_set('session.gc_maxlifetime', $lifetime);
+		ini_set('session.cookie_lifetime', $cookie_life);
 		ini_set('session.use_trans_sid', 0);
 		ini_set('session.use_only_cookies', 1);
 
-		// Is this->session is null
 		session_start();
 	}
-	
-	public static function get($var)
+
+	public static function getID()
 	{
-		return self::$memcache->get($var);
-	}
-	
-	public static function set($var, $value, $compress = false, $duration = 3600)
-	{
-		$compress = $compress ? MEMCACHE_COMPRESSED : 0;
-		self::$memcache->set($var, $value, $compress, time() + $duration);
-	}
-	
-	
-	public static function delete($var)
-	{
-		self::$memcache->delete($var);
+		if (!(boolean)session_id()) {
+			self::init();
+		}
+
+		return session_id();
 	}
 
 }
